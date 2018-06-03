5fea12a1ef41feb68350cb0a83fff0bce5c9d2ce
whidi <36885695+whidi@users.noreply.github.com>
2018-06-03 21:04:07 +0800

    Update App.js

--- App.js
+++ App.js
@@ -1,126 +1,122 @@
-import React, { Component } from "react";
-import { AppRegistry, StyleSheet, Dimensions, Image, View, StatusBar, TouchableOpacity, Text } from "react-native";
-
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
 import MapView from 'react-native-maps';
-import Polyline from '@mapbox/polyline';
+import { Marker } from 'react-native-maps';
+export default class App extends Component {
 
+  state = {
+    region: {
+      latitude: -8.149407,
+      longitude: 115.216667,
+      latitudeDelta: 0.8922,
+      longitudeDelta: 0.8421,
+    },
+    markers : [
+      {
+        key:1,
+        latlng: {
+          latitude:-8.675855,
+          longitude: 115.238384
+        },
+        title: 'Indomaret Tukad Yeh Aya',
+        subtitle: ''
+      },
+      {
+        key:2,
+        latlng: {
+          latitude:-8.676279,
+          longitude:  115.217344,
+        },
+        title: 'Indomaret Tukad Banyuasri',
+        subtitle: ''
+      },
+      {
+        key:3,
+        latlng: {
+          latitude:-8.678739, 
+          longitude: 115.214255
+        },
+        title: 'Dauh Puri Kelod'
+      }
+      ,
+      {
+        key:4,
+        latlng: {
+          latitude:-8.683320, 
+          longitude: 115.213997
+        },
+        title: 'Sesetan'
+      }
+   ]
+  };
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      latitude: null,
-      longitude: null,
-      error: null,
-      concat: null,
-      coords:[],
-      x: 'false',
-      cordLatitude:-8.23,
-      cordLongitude:115.02,
-    };
-
-    this.mergeLot = this.mergeLot.bind(this);
-
-  }
-
-  componentDidMount() {
-    navigator.geolocation.getCurrentPosition(
-       (position) => {
-         this.setState({
-           latitude: position.coords.latitude,
-           longitude: position.coords.longitude,
-           error: null,
-         });
-         this.mergeLot();
-       },
-       (error) => this.setState({ error: error.message }),
-       { enableHighAccuracy: false, timeout: 200000, maximumAge: 1000 },
-     );
-
-   }
-
-  mergeLot(){
-    if (this.state.latitude != null && this.state.longitude!=null)
-     {
-       let concatLot = this.state.latitude +","+this.state.longitude
-       this.setState({
-         concat: concatLot
-       }, () => {
-         this.getDirections(concatLot, "-8.239648,115.024726");
-       });
-     }
-
-   }
-
-   async getDirections(startLoc, destinationLoc) {
-
-         try {
-             let resp = await fetch(`https://maps.googleapis.com/maps/api/directions/json?origin=${ startLoc }&destination=${ destinationLoc }`)
-             let respJson = await resp.json();
-             let points = Polyline.decode(respJson.routes[0].overview_polyline.points);
-             let coords = points.map((point, index) => {
-                 return  {
-                     latitude : point[0],
-                     longitude : point[1]
-                 }
-             })
-             this.setState({coords: coords})
-             this.setState({x: "true"})
-             return coords
-         } catch(error) {
-           console.log('masuk fungsi')
-             this.setState({x: "error"})
-             return error
-         }
-     }
   render() {
-
     return (
-      <MapView style={styles.map} initialRegion={{
-        latitude: 37.78825,
-        longitude: -122.4324,
-        latitudeDelta: 0.0922,
-        longitudeDelta: 0.0421,
-      }}>
-
-      {!!this.state.latitude && !!this.state.longitude && <MapView.Marker
-         coordinate={{"latitude":this.state.latitude,"longitude":this.state.longitude}}
-         title={"Your Location"}
-       />}
-
-       {!!this.state.cordLatitude && !!this.state.cordLongitude && <MapView.Marker
-          coordinate={{"latitude":this.state.cordLatitude,"longitude":this.state.cordLongitude}}
-          title={"Your Destination"}
-        />}
-
-       {!!this.state.latitude && !!this.state.longitude && this.state.x == 'true' && <MapView.Polyline
-            coordinates={this.state.coords}
-            strokeWidth={2}
-            strokeColor="red"/>
-        }
-
-        {!!this.state.latitude && !!this.state.longitude && this.state.x == 'error' && <MapView.Polyline
-          coordinates={[
-              {latitude: this.state.latitude, longitude: this.state.longitude},
-              {latitude: this.state.cordLatitude, longitude: this.state.cordLongitude},
-          ]}
-          strokeWidth={2}
-          strokeColor="red"/>
-         }
-      </MapView>
+      <View style={styles.contMain}>
+        <View style={styles.contHeader}>
+            <Text style={styles.textHeader}>
+              Indomaret di Bali
+            </Text>
+        </View>
+        <View style={styles.contMaps}>
+              <MapView
+                style={styles.map}
+                region={this.state.region}
+              >
+              {this.state.markers.map(mark => (
+              <Marker
+                  key = {mark.key}
+                  coordinate={mark.latlng}
+                  title={mark.title}
+                  description={mark.subtitle}
+                />
+              ))}
+              </MapView>
+        </View>
+        <View style={styles.contFooter}>
+           <Text style={styles.textFooter}> http://whidigede@gmail.com </Text>
+        </View>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
   map: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
+    ...StyleSheet.absoluteFillObject,
   },
-});
+  contMain: {
+      flex : 1
+  },
+  contHeader: {
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    position: 'relative'
+  },
+  contMaps : {
+    flex : 10
+  },
+  textHeader: {
+    fontSize: 20,
+    color: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  contFooter: {
+    backgroundColor: 'green',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flex: 1,
+    position: 'relative'
+  },
+  textFooter: {
+    fontSize: 16,
+    color: 'white',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 
-export default App;
+});
